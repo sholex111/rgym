8c307996d51709cadf7c16260468db8aeba26c3f
olusola <sholex111@gmail.com>
2024-09-05 04:08:42 +0100

    rgym 04 lm

---
+++ sh_gym_04.R
@@ -0,0 +1,89 @@
+#Sh Gym04 Linear regression and outputs.
+
+
+library(tidyverse)
+library(here)
+library(janitor)
+library(readxl)
+library(broom)
+
+df = read_xlsx('Th_An_Test_2.xlsx', sheet = 1, range = "A5:AM34", col_names = FALSE)
+
+
+# Read the headers separately
+headers <- read_excel("Th_An_Test_2.xlsx", sheet = 2, range = "A6:AM6", col_names = FALSE)
+
+
+#add headers to df
+names(df) <- as_tibble(headers)
+
+#Clean the data
+
+# Ensure unique column names
+colnames(df) <- make.unique(colnames(df))
+
+# Remove columns with name "_"
+df <- df %>% select(-matches("^_"))
+
+
+#clean column names
+df <-  df %>% clean_names()
+
+#create new decile column
+df <- df %>%
+  mutate(
+    decile_1 = str_extract(decile, "\\d+")
+  )
+
+#rename long column names
+df <- df %>% rename("all_death_rate" = "x5_month_march_to_july_rate" )
+
+#check structure of the data
+str(df)
+
+#convert decile 1 to factor and cause of death to factor
+df <- df %>%
+  mutate(
+    decile_1 = as.factor(decile_1)
+  )
+
+
+#convert cause_of_death to factor
+df <- df %>%
+  mutate(
+    cause_of_death = as.factor(cause_of_death)
+  )
+
+
+#build model
+
+model <- lm(all_death_rate ~ cause_of_death + decile_1, data = df)
+summary(model)
+
+
+#Output the model to an excel sheet
+
+# Use broom's tidy function to get the model summary as a data frame
+tidy_summary <- tidy(model)
+
+# Get additional model statistics like R-squared, F-statistic, etc.
+tidy_model_stats <- glance(model)
+
+#transpose for a better view
+tidy_model_stats <- as.data.frame(t(tidy_model_stats)) 
+
+#further cleaning
+tidy_model_stats <- cbind(rownames(tidy_model_stats), estimate = tidy_model_stats)
+row.names(tidy_model_stats) <- NULL
+colnames(tidy_model_stats) <-  c("Term", "Estimates")
+
+
+
+# Combine the two: model summary and detailed result
+combined_model <- rbind(tidy_summary, tidy_model_stats)
+
+
+
+
+
+
