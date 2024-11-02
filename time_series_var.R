66e53c678c3b597417c79ae8a9f35f39a10efb76
Olusola <sholex111@gmail.com>
2024-11-02 18:34:37 +0000

    time_series with one var
    Time series with no external variables using ARIMA and ETS

--- time_series_var.R
+++ time_series_var.R
@@ -9,7 +9,11 @@ library(dplyr)
 
 
 ######
-# Assuming 'data' is your data frame and 'Emergency_Admission' is the admissions column
+
+## load data
+data <-  read.csv("hospital_emergency_data.csv")
+
+## Create time-series object
 admissions_ts <- ts(data$Emergency_Admission, start = c(data$Year[1], data$Month[1]), frequency = 12)
 
 # Step 2: Visualize the Data
@@ -58,6 +62,66 @@ summary(arimax_model)
 # if (exists("arimax_forecast")) {
 #   arimax_forecasted_values <- arimax_forecast$mean
 # }
+
+
+
+
+
+
+#####
+## Use ETS model
+
+## Fit ETS Model
+
+ets_model <- ets(admissions_ts)
+summary(ets_model)
+
+
+## Forecast the Next 12 Months
+ets_forecast <- forecast(ets_model, h = 12)
+plot(ets_forecast, main = "ETS Forecast for Emergency Admissions")
+
+# Optional: Save Forecasted Values
+ets_forecasted_values <- ets_forecast$mean
+ets_forecasted_values
+
+
+## Evaluate and Interpret the Forecast
+print(ets_forecast)
+
+
+#####
+## Some exploration
+
+avg_adm <- data %>% summarise(avg_mth = mean(Emergency_Admission), .by = Month)
+
+arima_forecasted_df <- as.data.frame(arima_forecasted_values)
+
+ets_forecasted_df <- as.data.frame(ets_forecasted_values)
+
+
+## combime the dataframes
+
+# Add Month column to arima_forecasted_df and ets_forecasted_df
+arima_forecasted_df <- arima_forecasted_df %>%
+  mutate(Month = 1:12)
+
+ets_forecasted_df <- ets_forecasted_df %>%
+  mutate(Month = 1:12)
+
+# Rename columns in arima_forecasted_df and ets_forecasted_df for clarity
+colnames(arima_forecasted_df)[1] <- "arima_forecast"
+colnames(ets_forecasted_df)[1] <- "ets_forecast"
+
+
+# Now, join all three data frames by Month
+combined_df <- avg_adm %>%
+  left_join(arima_forecasted_df, by = "Month") %>%
+  left_join(ets_forecasted_df, by = "Month")
+
+# View the combined data frame
+print(combined_df)
+
 
 
 
